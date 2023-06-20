1af6f7227d41c02343f0b562e443e7fb9650f858
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-06-20 15:17:20 +0300

    add mail regex validation

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,5 @@
 const phoneNumberRegex = /^[(]\d{3}[)][ ]\d{3}[-]\d{4}$/;
 
-const emailRegex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/gim;
+const emailRegex = /^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/;
 
 module.exports = { phoneNumberRegex, emailRegex };
