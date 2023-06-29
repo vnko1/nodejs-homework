f93620fac4611cc51c291ed79532c914e1933e2f
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-06-29 22:06:04 +0300

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
-app.use("/api/users", usersRouter);
+app.use("/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
 app.use((_, res) => {
