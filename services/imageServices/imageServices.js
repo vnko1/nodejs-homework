1ba5d5dc43605c07feac81e80ed0339d5e759fa4
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-06-29 10:45:40 +0300

    save

--- services/imageServices/imageServices.js
+++ services/imageServices/imageServices.js
@@ -1,7 +1,6 @@
 const Jimp = require("jimp");
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs/promises");
 const cloudinary = require("cloudinary").v2;
 const { ApiError } = require("../../utils");
 const { CLOUD_NAME, API_KEY, API_SECRET } = process.env;
