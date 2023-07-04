5b4f5c7da5860e035210eba39396bcd963add118
Andrii Valenko <102794464+vnko1@users.noreply.github.com>
2023-07-04 23:59:21 +0300

    save

--- app.js
+++ app.js
@@ -18,6 +18,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.render("hello");
+});
+
 app.use("/users", usersRouter);
 app.use("/api/contacts", contactsRouter);
 
